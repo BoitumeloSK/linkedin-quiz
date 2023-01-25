e6cd57274d61530b7ad8493fa44173b03be2f5d1
unknown <boitumelo75@gmail.com>
2023-01-25 20:29:55 +0200

    Message for selected answer fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,55 @@
 var inputDiv = $("#input-div").css("display", "none");
 var quizComplete = $("#quiz-complete").css("display", "none");
 var resultsDiv = $("#results-div").css("display", "none");
+var messageDiv = $("#message").css("display", "none");
 $("#quiz")
   .css("display", "flex")
   .css("flex-direction", "column")
   .css("align-items", "center");
 
-  //put options in array 
+//put options in array
 var questions = [
   {
     question: "2 X 4 = ",
-    answers: [6,7,8,9],
+    answers: [6, 7, 8, 9],
     correct: 8,
   },
 
   {
     question: "36 / 6 = ",
-    answers: [18,6,12,8],
+    answers: [18, 6, 12, 8],
     correct: 6,
   },
 
   {
     question: "102 + 2(12 - 4) = ",
-    answers: [105,124,832,118],
+    answers: [105, 124, 832, 118],
     correct: 118,
   },
 
   {
     question: "1 + 48 / 12 X 3 = ",
-    answers: [13,12.25,1.36,14],
+    answers: [13, 12.25, 1.36, 14],
     correct: 13,
   },
   {
     question: "1 + 1 = ",
-    answers: [4,3,2,1],
+    answers: [4, 3, 2, 1],
     correct: 2,
   },
 ];
 var count = 0;
 var answers = questions[count].answers;
 
-function labels(){
-  $("#label1").text(answers[0])
+function labels() {
+  $("#label1").text(answers[0]);
+  $("#first").val(answers[0]);
   $("#label2").text(answers[1]);
+  $("#second").val(answers[1]);
   $("#label3").text(answers[2]);
+  $("#third").val(answers[2]);
   $("#label4").text(answers[3]);
+  $("#fourth").val(answers[3]);
 }
 
 var startBtn = $("<button>").text("Start").css("cursor", "pointer");
@@ -54,9 +59,7 @@ testDiv.append(startBtn);
 startBtn.on("click", function () {
   var responses = [];
   startBtn.css("display", "none");
-  inputDiv
-    .css("display", "block")
-    .css("width", "80%")
+  inputDiv.css("display", "block").css("width", "80%");
   var headingDiv = $("<div>")
     .css("background-color", "grey")
     .css("text-align", "center");
@@ -67,7 +70,15 @@ startBtn.on("click", function () {
 
   var displayQuestion = $("<div>").css("text-align", "center");
   var question = $("<h4>").text(questions[count].question);
-  
+  var rightAnswer = $("<p>")
+    .text("Correct")
+    .css("color", "green")
+    .css("text-align", "center");
+  var wrongAnswer = $("<p>")
+    .text("Incorrect")
+    .css("color", "red")
+    .css("text-align", "center");
+
   var checkBtn = $("<button>").text("Check Answer");
   var nextBtn = $("<button>").text("Next").css("display", "none");
 
@@ -76,36 +87,72 @@ startBtn.on("click", function () {
   quizComplete.css("display", "block");
   resultsDiv.css("display", "flex");
 
-  labels()
+  labels();
 
   resultsDiv.append(checkBtn);
-
-  //check answer btn
-  checkBtn.on("click", function(){
-    checkBtn.css("display", "none");
-    nextBtn.css("display", "block");
-  });
-
-  //next btn
-  nextBtn.on("click", function () {
+  
+  /** Moves on to the next question after 5 seconds (test) - currently only works for 1st question*/
+  function nextQuestion() {
     nextBtn.css("display", "none");
     checkBtn.css("display", "block");
+    messageDiv.css("display", "none");
 
     $("input[type=radio]").prop("checked", false);
 
     count = count + 1;
     answers = questions[count].answers;
-    labels()
+    labels();
     question.text(questions[count].question);
+  }
+  // setTimeout(() => {
+  //   nextQuestion();
+  // }, 5000);
 
-    if(count > 4)
-    {
-      
-    }
+
+  //check answer btn
+  checkBtn.on("click", function () {
+    checkBtn.css("display", "none");
+    nextBtn.css("display", "block");
+    messageDiv.css("display", "flex");
+
+    var checkedNumber = $("input[type=radio]:checked").val();
+    //var isChecked = $("input[type=radio]:checked"
+
+    var x = questions[count].correct
+
+      if (checkedNumber == x) {
+        messageDiv.append(rightAnswer);
+      } else {
+        messageDiv.append(wrongAnswer);
+      }
+  });
+
+  //next btn
+  nextBtn.on("click", function () {
+    var checkedNumber = $("input[type=radio]:checked").val();
+    nextQuestion()
+
+    questions.forEach((question)=>{
+      if($("input[type=radio]:checked"))
+      {
+        responses.push(checkedNumber)
+      }
+    })
+//     if(count > 4)
+//     {
+//       testDiv.css("display", "none")
+// //    inputDiv.css("display", "none")
+// //             quizComplete.css("display", "block")
+
+// //             var congratulations = $("<h1>").text("Quiz Complete")
+// //             quizComplete.append(congratulations)
+
+// //             var answered = $("<p>").text(answers.length)
+// //             quizComplete.append(answered)
+//     }
   });
   resultsDiv.append(nextBtn);
 });
-
 
 // startBtn.on("click", function(){
 
@@ -162,3 +209,12 @@ startBtn.on("click", function () {
         var error = $("<p>").text("Incorrect").css("color", "red");
         quizComplete.append(error);
       } */
+
+       //if(checkedNumber != question.correct)
+      // else
+      // {
+      //   count = count
+      //   console.log
+      //   var message = $("<p>").text("Please select an answer")
+      //   messageDiv.append(message)
+      // }
